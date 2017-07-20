6a2413f996c09802de2dc47a640c88b1f7297804
root <>
2017-07-20 04:31:02 +0200

    fix

--- make_mksquashfs.sh
+++ make_mksquashfs.sh
@@ -97,7 +97,7 @@ then
   # module and hooks
   echo "MODULES=\"i915 radeon\"" > ${work_dir}/${arch}/airootfs/etc/mkinitcpio.conf
   echo "HOOKS=\"base udev block filesystems keyboard archiso\"" >> ${work_dir}/${arch}/airootfs/etc/mkinitcpio.conf
-  echo "COMPRESSION=\"cat\"" >> ${work_dir}/${arch}/airootfs/etc/mkinitcpio.conf
+  echo "COMPRESSION=\"xz\"" >> ${work_dir}/${arch}/airootfs/etc/mkinitcpio.conf
 
   # modprobe.d
   mkdir -p ${work_dir}/${arch}/airootfs/etc/modprobe.d/

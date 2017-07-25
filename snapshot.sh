b9d5f35e2d557f9f28debc8f3682df5b3575c25b
root <simonrieger1999@t-online.de>
2017-07-25 19:41:55 +0200

    fix

--- snapshot.sh
+++ snapshot.sh
@@ -5,16 +5,13 @@ set -ex
 if [ "make" == "$1" ]; then
 
 echo `date "+%Y%m%d-%H%M%S"` > /run/btrfs-root/__current/ROOT/SNAPSHOT
-echo "Fresh install" >> /run/btrfs-root/__current/ROOT/SNAPSHOT
+echo "${2}" >> /run/btrfs-root/__current/ROOT/SNAPSHOT
 
-btrfs subvolume snapshot -r /run/btrfs-root/__current/ROOT
-                            /run/btrfs-root/__snapshot/ROOT@`head -n 1 /run/btrfs-root/__current/ROOT/SNAPSHOT`
+btrfs subvolume snapshot -r /run/btrfs-root/__current/ROOT /run/btrfs-root/__snapshot/ROOT@`head -n 1 /run/btrfs-root/__current/ROOT/SNAPSHOT`
 
 rm /run/btrfs-root/__current/ROOT/SNAPSHOT
 
-fi
-
-if [ "restore" == "$1" ]; then
+elif [ "restore" == "$1" ]; then
 
 echo "Heutiges datum $(date "+%Y%m%d-%H%M%S")"
 ls /run/btrfs-root/__snapshot
@@ -23,5 +20,10 @@ read -p "Welches datum hat das Image? : " datum
 mv /run/btrfs-root/__current/ROOT /run/btrfs-root/__current/ROOT.old
 btrfs subvolume snapshot /run/btrfs-root/__snapshot/ROOT@${datum} /run/btrfs-root/__current/ROOT
 reboot
+
+else
+
+echo "bash ./snapshot.sh PARAMETER BESCHREIBUNG"
+echo "Parameters: make restore"
 
 fi

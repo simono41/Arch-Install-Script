714714b96ae9574b772f6f3c432bbe3b6320c460
simono41 <simono41>
2017-09-23 00:00:21 +0200

    fix

--- make_mksquashfs.sh
+++ make_mksquashfs.sh
@@ -230,14 +230,14 @@ function filesystem() {
         read -p "Soll das System aktualisiert werden? [Y/n] " update
         if [ "$update" != "n" ]
         then
+    arch-chroot ${work_dir}/${arch}/airootfs /bin/bash pacman -Sy archlinux-keyring && pacman -Su
+
     arch-chroot ${work_dir}/${arch}/airootfs /bin/bash <<EOT
 
 # initalizise keys
-#pacman-key --init
-#pacman-key --populate archlinux
+pacman-key --init
+pacman-key --populate archlinux
 #pacman-key --refresh-keys
-
-pacman -Sy archlinux-keyring && pacman -Su
 
 #mkdir -p /etc/systemd/system/getty\@tty1.service.d
 #echo "[Service]" > /etc/systemd/system/getty\@tty1.service.d/autologin.conf

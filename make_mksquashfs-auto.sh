3b3fe5f037239bab8022a11369ce88f9f2325902
simono41 <simono41>
2018-04-25 11:19:45 +0200

    fix

--- make_mksquashfs-auto.sh
+++ make_mksquashfs-auto.sh
@@ -140,12 +140,6 @@ function system() {
                 echo "COMPRESSION=\"lz4\"" >> ${work_dir}/${arch}/airootfs/etc/mkinitcpio.conf
                 echo "FILES=\"/etc/modprobe.d/blacklist-floppy.conf\"" >> ${work_dir}/${arch}/airootfs/etc/mkinitcpio.conf
 
-                if [ "${version}" == "libre" ]; then
-                    ./arch-chroot ${work_dir}/${arch}/airootfs mkinitcpio -p linux-libre
-                else
-                    ./arch-chroot ${work_dir}/${arch}/airootfs mkinitcpio -p linux
-                fi
-
             else
                 #echo "MODULES=\"i915 radeon ata_generic ata_piix nls_cp437 vfat ext4 btrfs\"" > ${work_dir}/${arch}/airootfs/etc/mkinitcpio.conf
                 #echo "HOOKS=\"base udev plymouth memdisk archiso_shutdown archiso archiso_loop_mnt archiso_pxe_common archiso_pxe_nbd archiso_pxe_http archiso_pxe_nfs archiso_kms block pcmcia filesystems keyboard\"" >> ${work_dir}/${arch}/airootfs/etc/mkinitcpio.conf
@@ -156,8 +150,6 @@ function system() {
                 echo "HOOKS=\"base udev archiso block filesystems keyboard\"" >> ${work_dir}/${arch}/airootfs/etc/mkinitcpio.conf
                 echo "COMPRESSION=\"lz4\"" >> ${work_dir}/${arch}/airootfs/etc/mkinitcpio.conf
                 echo "FILES=\"/etc/modprobe.d/blacklist-floppy.conf /etc/modprobe.d/blacklist_nouveau.conf\"" >> ${work_dir}/${arch}/airootfs/etc/mkinitcpio.conf
-
-                ./arch-chroot ${work_dir}/${arch}/airootfs mkinitcpio -p linux
 
             fi
         fi

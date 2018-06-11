95b088155c658416bff6222ce463fb937859e3d4
simono41 <simono41>
2018-06-12 00:32:01 +0200

    admin_fix

--- aurupdater.sh
+++ aurupdater.sh
@@ -2,6 +2,12 @@
 
 set -x
 
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root" 1>&2
+    sudo "$0" "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
+    exit 0
+fi
+
 username="user1"
 
 echo "Durchsuche auf neue Packete indem fremde Packete angezeigt werden!!!"

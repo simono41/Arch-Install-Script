a63b38e66a5a62e5385b95f3f81125a80206f31c
simono41 <simono41>
2018-08-23 14:07:45 +0200

    fix

--- make_mksquashfs-auto.sh
+++ make_mksquashfs-auto.sh
@@ -215,7 +215,7 @@ function BIOS() {
         cp -R ${work_dir}/${arch}/airootfs/usr/lib/syslinux/bios/* ${work_dir}/iso/${install_dir}/boot/syslinux/
 
         if [ "${version}" == "manjaro" ]; then
-          cp $(echo $(find ${work_dir}/${arch}/airootfs/boot/ -name "initramfs*$(uname -m).img")  | cut -d" " -f2 ) ${work_dir}/iso/${install_dir}/boot/${arch}/archiso.img
+          cp $(echo $(find ${work_dir}/${arch}/airootfs/boot/ -name "initramfs*$(uname -m).img")  | cut -d" " -f1 ) ${work_dir}/iso/${install_dir}/boot/${arch}/archiso.img
           cp $(echo $(find ${work_dir}/${arch}/airootfs/boot/ -name "vmlinuz*$(uname -m)")  | cut -d" " -f2 ) ${work_dir}/iso/${install_dir}/boot/${arch}/vmlinuz
         else
           cp ${work_dir}/${arch}/airootfs/boot/initramfs-linux${linuxparameter}.img ${work_dir}/iso/${install_dir}/boot/${arch}/archiso.img

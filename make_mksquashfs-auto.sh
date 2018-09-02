b458669754453b574dff56ed82d0f562f762bee6
simono41 <simono41>
2018-09-03 01:29:15 +0200

    fix

--- make_mksquashfs-auto.sh
+++ make_mksquashfs-auto.sh
@@ -62,7 +62,7 @@ EOT
         #pacman -U --noconfirm --force "https://www.parabola.nu/packages/libre/any/parabola-keyring/download"
         pacman -U --noconfirm --force "https://repomirror.parabola.nu/pool/parabola/parabola-keyring-20180802-1-any.pkg.tar.xz"
         #pacman -U --noconfirm --force "https://repomirror.parabola.nu/libre/os/x86_64/parabola-keyring-20180802-1-any.pkg.tar.xz"
-        ./pacstrap -C pacman.conf_libre -c -d -G -M ${work_dir}/${arch}/airootfs $(cat base_libre.txt)
+        ./pacstrap -C pacman.conf_libre -c -d -G -M ${work_dir}/${arch}/airootfs $(cat base_${version}.txt)
     elif [ "${version%-*-*}" == "manjaro" ]; then
         cp pacman.conf_manjaro /etc/pacman.conf
         pacman -U --noconfirm --force "https://mirror.philpot.de/manjaro/stable/core/x86_64/manjaro-keyring-20180607-1-any.pkg.tar.xz"
